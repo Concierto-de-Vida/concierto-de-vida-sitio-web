c37114cba2642a88cfa3efe636dd3b12b039977d
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-11-10 10:38:18 -0600

    Remove usless comments

--- types/EducationLevel.ts
+++ types/EducationLevel.ts
@@ -1,13 +1,3 @@
-// { id: "preschool", name: "Preescolar" },
-// { id: "primary", name: "Primaria" },
-// { id: "secondary", name: "Secundaria" },
-// { id: "highSchool", name: "Preparatoria o Bachillerato" },
-// { id: "technical", name: "Técnico o Comercial" },
-// { id: "bachelor", name: "Licenciatura" },
-// { id: "master", name: "Maestría" },
-// { id: "doctorate", name: "Doctorado" },
-// { id: "none", name: "Sin escolaridad" },
-
 export const EDUCATION_LEVELS = [
   "preschool",
   "primary",
