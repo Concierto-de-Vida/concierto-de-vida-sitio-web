470d4fc145ef6dd3b5babf2c620e98970c48cfb8
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-11-27 17:38:18 -0600

    Show error message when password is invalid

--- routes/login.tsx
+++ routes/login.tsx
@@ -1,28 +1,37 @@
 import State from "../types/state.type.ts";
-import { Handlers } from "$fresh/server.ts";
 import redirect from "../utils/redirect.ts";
 import Button from "../components/Button.tsx";
+import { AppProps, Handlers } from "$fresh/server.ts";
 import Typography from "../components/Typography.tsx";
 import { isTokenValid } from "../data/controllers/tokensController.ts";
 
-export const handler: Handlers<null, State> = {
+interface LoginProps {
+  lastAttemptStatus: false | undefined;
+}
+
+export const handler: Handlers<LoginProps, State> = {
   async POST(req, ctx) {
     const form = await req.formData();
 
     const authToken = form.get("authToken")?.toString();
     if (!authToken) return new Response("Missing auth token", { status: 401 });
-    if (!(await isTokenValid(authToken))) return new Response("Unauthorized", { status: 401 });
 
-    ctx.state.session.set("token", authToken);
+    if (await isTokenValid(authToken)) {
+      ctx.state.session.set("token", authToken);
+      ctx.state.session.flash("lastAttemptStatus");
+    } else {
+      ctx.state.session.flash("lastAttemptStatus", false);
+    }
 
     return redirect("/");
   },
+  GET: (_, ctx) => ctx.render({ lastAttemptStatus: ctx.state.session.flash("lastAttemptStatus") }),
 };
 
-export default function Login() {
+export default function Login({ data }: AppProps<LoginProps, State>) {
   return (
     <>
-      <Typography variant="h4" class="text-2xl">
+      <Typography variant="h4" class="text-2xl mb-5">
         Iniciar sesión
       </Typography>
 
@@ -37,6 +46,10 @@ export default function Login() {
             class="mt-2 p-2 border border-gray-300 rounded w-full"
           />
         </div>
+
+        {data.lastAttemptStatus === false && (
+          <Typography class="text-red-500">El token de autenticación es inválido</Typography>
+        )}
 
         <div class="mt-3 flex justify-center items-center">
           <Button class="mt-2 p-2" type="submit" color="green">
