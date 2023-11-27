30ca2290da84399c31a4e0857d16725ac3bc73ae
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-11-27 10:34:50 -0600

    Fix creating master token

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
-import { loadSync } from "$std/dotenv/mod.ts";
 import { hash } from "bcrypt";
-import { getToken } from "./data/controllers/tokensController.ts";
 import db from "./data/database.ts";
+import { loadSync } from "$std/dotenv/mod.ts";
+import { isTokenValid } from "./data/controllers/tokensController.ts";
 
 const env = loadSync({ examplePath: "./.example.env" });
 
@@ -10,7 +10,9 @@ export const MASTER_TOKEN = await hash(env.MASTER_TOKEN);
 Deno.env.set("APP_KEY", MASTER_TOKEN);
 
 // Add the token to the database if it doesn't exist
-if ((await getToken(MASTER_TOKEN)) === null)
+if (!(await isTokenValid(env.MASTER_TOKEN))) {
+  console.log("Creating master token...");
   await db.tokens.add({ token: MASTER_TOKEN, isAdmin: true, createdAt: new Date(), label: "Administrador" });
+}
 
 export const IS_PRODUCTION = Boolean(Deno.env.get("DENO_DEPLOYMENT_ID"));
